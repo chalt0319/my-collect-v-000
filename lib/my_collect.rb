37558f9d9ad2d1cbdaa07fda4216c2d342e3a6c7
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-18 12:59:12 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
-my_collect(empty_array) 
-  empty_array = []
-  counter = 0 
-  my_collect(empty_array) do |x|
-    counter += 1 
-  end 
+#my_collect(empty_array) 
+ # empty_array = []
+  #counter = 0 
+  #my_collect(empty_array) do |x|
+   # counter += 1 
+  #end 
 
+my_collect(languages) do |languages|
+  languages.upcase 
+end 
