d94a41aac3f396328b2c121ff232a6dbde8fb9d6
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-18 13:00:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,6 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
+
 #my_collect(empty_array) 
  # empty_array = []
   #counter = 0 
